a545ca19a580739995ced3f4052b7e1c07c8cdcd
orish25 <115727307+orish25@users.noreply.github.com>
2023-10-12 12:26:12 +0300

    Update mHSPC_OS_stampede_adjustment_RE.R

--- Base case/code/mHSPC_OS_stampede_adjustment_RE.R
+++ Base case/code/mHSPC_OS_stampede_adjustment_RE.R
@@ -5,6 +5,10 @@
 # Load the R2OpenBUGS package
 library(R2OpenBUGS)
 library(readxl)
+library(here)
+
+# Load the data 
+mHSPC_OS_data_PO <- read_excel(here("Base case/data", "mHSPC OS data PO.xlsx"))
 
 # Random effects model
 model_normal_identity_re <- function()
