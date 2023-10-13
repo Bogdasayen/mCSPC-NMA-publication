fccc55b6dea7a1f7c53353deef70cccca015be7a
orish25 <115727307+orish25@users.noreply.github.com>
2023-10-13 10:52:00 +0300

    Update mHSPC_PFS_NormId_FE_high risk.R

--- Subgroups/code/mHSPC_PFS_NormId_FE_high risk.R
+++ Subgroups/code/mHSPC_PFS_NormId_FE_high risk.R
@@ -5,7 +5,10 @@
 # Load the R2OpenBUGS package
 library(R2OpenBUGS)
 library(readxl)
-#mHSPC_OS_data <- read_excel("mHSPC OS data.xlsx")
+library(here)
+
+# Load the data 
+mHSPC_PFS_data_high_risk <- read_excel(here("Subgroups/data", "mHSPC PFS data high risk.xlsx"))
 
 # Normal likelihood, identity link, fixed effects
 model_normal_identity_fe <- function()
