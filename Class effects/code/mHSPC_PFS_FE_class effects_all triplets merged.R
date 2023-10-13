30927a7a070134cf00389089f7cce01ce2fe13f9
orish25 <115727307+orish25@users.noreply.github.com>
2023-10-13 08:04:49 +0300

    Update mHSPC_PFS_FE_class effects_all triplets merged.R

--- Class effects/code/mHSPC_PFS_FE_class effects_all triplets merged.R
+++ Class effects/code/mHSPC_PFS_FE_class effects_all triplets merged.R
@@ -5,7 +5,11 @@
 # Load the R2OpenBUGS package
 library(R2OpenBUGS)
 library(readxl)
-#mHSPC_OS_data <- read_excel("mHSPC PFS data PO.xlsx") 
+library(here)
+
+# Load the data 
+mHSPC_PFS_class_effects_all_triplets_merged <- read_excel(here("Class effects/data", "mHSPC PFS_class effects_all triplets merged.xlsx"))
+
 
 # Normal likelihood, identity link, fixed effects
 model_normal_identity_fe <- function()
@@ -203,13 +207,13 @@ num_sims <- 10000 * n_chains
 burn_in <- 10000 * n_chains	
 
 # Define the bugs data 
-ns <- nrow(mHSPC_PFS_class_effects)
-t  <- array(c(mHSPC_PFS_class_effects$t1, mHSPC_PFS_class_effects$t2), dim = c(ns, 2)) 
+ns <- nrow(mHSPC_PFS_class_effects_all_triplets_merged)
+t  <- array(c(mHSPC_PFS_class_effects_all_triplets_merged$t1, mHSPC_PFS_class_effects_all_triplets_merged$t2), dim = c(ns, 2)) 
 nt <- max(t) 
-y  <- array(c(rep(0, ns), mHSPC_PFS_class_effects$y), dim = c(ns, 2))
-se <- array(c(rep(0, ns), mHSPC_PFS_class_effects$se), dim = c(ns, 2))
+y  <- array(c(rep(0, ns), mHSPC_PFS_class_effects_all_triplets_merged$y), dim = c(ns, 2))
+se <- array(c(rep(0, ns), mHSPC_PFS_class_effects_all_triplets_merged$se), dim = c(ns, 2))
 
-study_names <- gsub("#", "", mHSPC_PFS_class_effects$`#ID`)
+study_names <- gsub("#", "", mHSPC_PFS_class_effects_all_triplets_merged$`#ID`)
 rownames(t) <- rownames(y) <- rownames(se) <- study_names
 
 
