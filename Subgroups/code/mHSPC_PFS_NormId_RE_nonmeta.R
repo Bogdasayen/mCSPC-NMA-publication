f5208ce76757ba5e2a91dcdc6db5c1ad84d4afaf
orish25 <115727307+orish25@users.noreply.github.com>
2023-10-13 11:00:01 +0300

    Update mHSPC_PFS_NormId_RE_nonmeta.R

--- Subgroups/code/mHSPC_PFS_NormId_RE_nonmeta.R
+++ Subgroups/code/mHSPC_PFS_NormId_RE_nonmeta.R
@@ -3,6 +3,10 @@
 # Load the R2OpenBUGS package
 library(R2OpenBUGS)
 library(readxl)
+library(here)
+
+# Load the data 
+mHSPC_PFS_data_non_meta <- read_excel(here("Subgroups/data", "mHSPC PFS data non_meta.xlsx"))
 
 # Normal likelihood, identity link, random effects
 model_normal_identity_re <- function()
