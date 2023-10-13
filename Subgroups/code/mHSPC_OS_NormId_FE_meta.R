8cd2f5d0390c882765fa2834510ca989d8061e45
orish25 <115727307+orish25@users.noreply.github.com>
2023-10-13 10:36:36 +0300

    Update mHSPC_OS_NormId_FE_meta.R

--- Subgroups/code/mHSPC_OS_NormId_FE_meta.R
+++ Subgroups/code/mHSPC_OS_NormId_FE_meta.R
@@ -5,7 +5,10 @@
 # Load the R2OpenBUGS package
 library(R2OpenBUGS)
 library(readxl)
-#mHSPC_OS_data <- read_excel("mHSPC OS data.xlsx")
+library(here)
+
+# Load the data 
+mHSPC_OS_data_meta <- read_excel(here("Subgroups/data", "mHSPC OS data meta.xlsx"))
 
 # Normal likelihood, identity link, fixed effects
 model_normal_identity_fe <- function()
