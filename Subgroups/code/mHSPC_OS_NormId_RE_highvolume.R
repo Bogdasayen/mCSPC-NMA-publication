c3108355f459d0b0941323bdbd9394bee0b94900
orish25 <115727307+orish25@users.noreply.github.com>
2023-10-13 10:32:03 +0300

    Update mHSPC_OS_NormId_RE_highvolume.R

--- Subgroups/code/mHSPC_OS_NormId_RE_highvolume.R
+++ Subgroups/code/mHSPC_OS_NormId_RE_highvolume.R
@@ -3,6 +3,10 @@
 # Load the R2OpenBUGS package
 library(R2OpenBUGS)
 library(readxl)
+library(here)
+
+# Load the data 
+mHSPC_OS_data_high_volume <- read_excel(here("Subgroups/data", "mHSPC OS data high_volume.xlsx"))
 
 # Normal likelihood, identity link, random effects
 model_normal_identity_re <- function()
